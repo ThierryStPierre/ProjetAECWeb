4580f0538872eb63f32ec2db8ab94a35d1dcc635
joblo8 <Jonathan.Bleau-Guerette.1@collegeahuntsic.qc.ca>
2016-04-06 14:00:11 -0400

    Update Ligue.sql

--- SQL/Ligue.sql
+++ SQL/Ligue.sql
@@ -1,6 +1,8 @@
 create table Ligue(
 	ID_Ligue int NOT NULL AUTO_INCREMENT, 
-	Nom_Ligue varchar(40)NOT NULL,
+	ID_Gestionnaire int NOT NULL,
+	Nom_Ligue varchar(40) NOT NULL,
 	UNIQUE (ID_Ligue),
-	PRIMARY KEY(ID_Ligue)
+	PRIMARY KEY(ID_Ligue),
+	FOREIGN KEY (ID_Gestionnaire) REFERENCES Joueur (ID_Joueur)
 );
