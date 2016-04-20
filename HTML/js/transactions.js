c54da98c5b5df7f6e603e0c0905b0ea8cb2b3494
chautran <Tran-Minh-Lam.Chau.1@collegeahuntsic.qc.ca>
2016-04-20 17:01:45 -0400

    Added files via upload
    ajouter les validations des champs

--- HTML/js/transactions.js
+++ HTML/js/transactions.js
@@ -37,7 +37,7 @@
 				var cat_ligue = $("#category_name").val();
 					
 				if (nom_ligue == "" || nom_sligue == "" || cat_ligue == "" ) {
-					alert("Les champs de ligue ne sont pas rempli");
+					alert("Les champs de ligue sont requis");
 					return false;
 				}
 		
@@ -60,8 +60,7 @@
 						alert(JSON.stringify(reponse));		
 					}
 				});
-				
-				
+
 				alert("La ligue " +nom_ligue +" est créée avec succes");
 				
 				$("#ligue").hide();
@@ -85,7 +84,7 @@
 				var nb_joueur = $("#nombre_joueur").val();
 				
 				if (nb_equipe == "" || nom_equipe == "" || nb_joueur == "" ) {
-					alert("Les champs d'équipe ne sont pas rempli");
+					alert("Les champs d'équipe sont requis");
 					return false;
 				}
 				
@@ -109,7 +108,6 @@
 					}
 				});
 
-
 				alert("L'équipe " +nom_equipe +" est créé avec succes");
 		
 				$("#equipe").hide();
@@ -117,10 +115,8 @@
 				return true;
 			}
 
-
-
-			
-			
+			var message = "";
+			var prompt = 0;
 			var joueur =[];
 			function click_inscription() {
 			/* (pour test) ceci devrait implanter une fois idLigue est transféré */
@@ -130,7 +126,6 @@
 				document.form_inscription.pointeur.value = true;
 				document.form_inscription.gestionnaire.value = true;
 				
-			
 				/* saisir les entrees d'inscription */
 				var nom_joueur = $("#nom").val();
 				var prenom_joueur = $("#prenom").val();
@@ -140,34 +135,62 @@
 				var pos_joueur = $("#position").val();
 				var chand_joueur = $("#numeroChandail").val();
 				var nb_joueur = $("#nombre_joueur").val(); /* valeur du panneau d'equipe */
+				var nombre = document.form_inscription.telephone.value;
+				var chiffres = new String(nombre);
+				var phoneno = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+				var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+				var num_chandail = document.form_inscription.numeroChandail.value;
 				
 				var data_joueur = null;
 				var str = "";
 				var Jlenght;
+				
+				if (nom_joueur == "" || prenom_joueur == "" || ddn_joueur == "" || tel_joueur == "" || email_joueur == "" || pos_joueur == "" || chand_joueur == "") {
+					alert("Les champs d'inscription joueur sont requis");
+					return false;
+				}
+				
+				/* valider la date */
+				var dateInput = document.getElementById('dateDeNaissance').value;
+				validerDate(dateInput);
+				
+				if (prompt == 1) {
+					prompt = 0;
+					alert(message);
+					document.form_inscription.dateDeNaissance.focus();
+					return false;					
+				}
+				
+				if(!document.form_inscription.telephone.value.match(phoneno))
+				{  /* valider le telephone */
+					alert("Assurez-vous de rentrer un numéro à 10 chiffres (xxx-xxx-xxxx)"); 
+					document.form_inscription.telephone.focus(); 
+					return false;  
+				}  
+				
+				if (!document.form_inscription.courriel.value.match(mailformat))  
+				{  /* valider le courriel */
+					alert("Vous avez entré un courriel non valide !\nRéentrez votre courriel, SVP");  
+					document.form_inscription.courriel.focus(); 
+					return false;
+				}
+
+				if (isNaN(parseInt(num_chandail))) //num_chandail n'est pas un nombre
+				{  /* valider le numero Chandail */
+					alert("Vous avez entré un numero Chandail non valide !\nRéentrez votre numero Chandail, SVP");
+					document.form_inscription.numeroChandail.focus();
+					return false;
+				}
 
 				data_joueur = nom_joueur +", " +prenom_joueur +", " +ddn_joueur +", " +tel_joueur +", "
 				+email_joueur +", " +pos_joueur +", " +", " +chand_joueur +".";
-				
+
 				joueur.push(data_joueur);	
 				Jlenght = joueur.length;
 				str = document.getElementById("table_joueur");
-				
-				/* colorer pour faciliter la visualisation */
-		/*	---	for (i=0; i < joueur.length; i++) {
-					if (Jlenght%2 == 0 ) {
-						str.innerHTML = joueur[i] +"<br />";
-						str.style.color = 'red' ;  //'#40ff00';
-					}
-					else {
-						str.innerHTML = joueur[i] +"<br />";
-						str.style.color = 'green';  //'#ffbf00';
-					}
-				}
-		--- */
-			
-			
 				str.innerHTML = joueur.join('<br />'); // afficher dans la boite du droit
-			
+
+				
 				alert($('#form_inscription').serialize());
 
 				$.ajax({
@@ -190,7 +213,6 @@
 
 				alert("Le joueur " +nom_joueur +" est créé avec succes");
 		
-				
 				/* remettre a blanc les champs d'entree */
 				document.getElementById('nom').value = '';
 				document.getElementById('prenom').value = '';
@@ -214,8 +236,57 @@
 
 			}
 			
-			function click_quitter() {
-				exit();
+			function click_quitter(){
+				window.close();
 			}
 			
+			function validerDate(maDate) {
+				var dateformat =/^([0-9]{4})\/(0?[1-9]|1[0-2])\/(0?[1-9]|1[0-9]|2[0-9]|3[0-1])$/;  // format YYYY/MM/DD
+				var ListofDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+				var thedate, day, month, year;
+				thedate = maDate.split('/');
+			
+				year = parseInt(thedate[0]);
+				month = parseInt(thedate[1]);
+				day = parseInt(thedate[2]);
+			
+				if (maDate.match(dateformat)) {
+					if (month == 1 || month > 2) {
+						if (day > ListofDays[month - 1]) {
+							message = 'La date est non valide!';
+							prompt = 1; 
+							return false;
+						}
+					}
+				
+					if (month == 2) {
+						var leapYear = false;
+						
+						if ((!(year % 4) && year % 100) || !(year % 400)) {
+							leapYear = true;
+						}
+
+						if ((leapYear == false) && (day >= 29)) {
+							message = "L'année n'est pas bissectile!\nLa date doit être le 28";
+							prompt = 1; 
+							return false;
+						}
+
+						if ((leapYear == true) && (day > 29)) {
+							message = 'La date est non valide!';
+							prompt = 1; 
+							return false;
+						}
+					}
+				}
+				else {
+					message = "Vous avez entré une date avec format non valide !\nFormat yyyy/mm/dd (ex.: 2016/01/31)"; 
+					prompt = 1;
+					return false;
+				}
+			}
+			
+			
+			
+				
 			
