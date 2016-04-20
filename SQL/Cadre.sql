ae0e4b042ed6c65e614113477c7a7da7cb551d3a
ThierryStPierre <Thierry.St-Pierre.1@collegeahuntsic.qc.ca>
2016-04-20 09:36:59 -0400

    Delete Cadre.sql

--- SQL/Cadre.sql
+++
@@ -1,8 +0,0 @@
-create table Cadre(
-	ID_Cadre int, /* Est-ce Utile?*/
-	ID_Ligue int NOT NULL,
-	ID_Personne int NOT NULL, 
-	ID_Saison int NOT NULL
-	,UNIQUE (ID_Cadre),
-	PRIMARY KEY(ID_Cadre)
-);
